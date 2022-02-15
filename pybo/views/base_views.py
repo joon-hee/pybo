149ed359d50ce7517ab48ff8326c5efc2142c432
김준희 <joonhee@gmail.com>
2022-02-15 15:11:43 +0900

    로그 출려전3

--- pybo/views/base_views.py
+++ pybo/views/base_views.py
@@ -1,13 +1,13 @@
 import logging
 
 from django.core.paginator import Paginator
+from django.db.models import Q, Count
 from django.shortcuts import render, get_object_or_404
 
-from django.db.models import Q, Count
 from ..models import Question
 
-
 logger = logging.getLogger('pybo')
+
 
 def index(request):
     logger.info("INFO 레벨로 출력")
